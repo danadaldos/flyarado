327201d714f3bb57887a955109f64742878caa7f
lanterma <lanterma@gmail.com>
2015-04-11 16:54:12 -0600

    Added links to about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   resources :rivers
 
+  get 'pages/about', as: 'about'
+  get 'pages/contact', as: 'contact'
+  get 'pages/home', as: 'home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
