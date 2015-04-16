61d381354e34c1de60885d2442ab15446046e54d
Piper Niehaus <piper.niehaus@gmail.com>
2015-04-16 13:44:58 -0600

    Update login from Jason’s comments * Make password field a password_field_tag * Create a flash message that appears on login
    [#91901578]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:id] = user.id
-      redirect_to root_path
+      redirect_to root_path,
+        notice: "Welcome back, #{user.first_name}"
     else
       flash[:error] = 'Invalid email or password'
       render :login
