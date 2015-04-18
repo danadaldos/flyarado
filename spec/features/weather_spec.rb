ae6d6fa3873083fb255b3175205c7cd7ea9b3469
Dana Daldos <danadaldos@gmail.com>
2015-04-18 15:45:26 -0600

    Committing most recent non-working spec.
    [#92272152]

--- spec/features/weather_spec.rb
+++ spec/features/weather_spec.rb
@@ -4,19 +4,17 @@ feature 'Weather on the homepage' do
   scenario 'can see weather current weather on show page' do
     visit '/'
 
-    click_link 'New River'
+    click_link 'New Station'
 
-    fill_in 'Station', with: 'Nofish River'
-    fill_in 'Cfs', with: '50000'
+    fill_in 'Name', with: 'Nofish River'
     fill_in 'Latitude', with: '39.7392'
     fill_in 'Longitude', with: '-104.9903'
 
-    click_button 'Create River'
+    click_button 'Create Station'
 
     expect(page).to have_content("Nofish River")
-    expect(page).to have_content("50000")
-    expect(page).to have_content(:temperature)
-    expect(page).to have_content(:summary)
+    expect("#w_temperature").to_not be_empty
+    expect('#w_summary').to_not eq("Overcast")
 
   end
 end
