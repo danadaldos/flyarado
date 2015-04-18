6499342b3d17bdef62a8bc28c2e2416a7deb57c1
Dana Daldos <danadaldos@gmail.com>
2015-04-18 14:57:44 -0600

    This is a re-do of the weather API  functionality integration post-Stations migrate.
    [#92272152]

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,2 +1,12 @@
 class Station < ActiveRecord::Base
+  def weather
+    @weather ||= begin
+      ForecastIO.api_key = '61abb8248f83b9d71325355d0b4fde8c'
+      @cast = ForecastIO.forecast(latitude, longitude)
+      {
+        temperature: @cast.currently.temperature,
+        summary: @cast.currently.summary
+      }
+    end
+  end
 end
