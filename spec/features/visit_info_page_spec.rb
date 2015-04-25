543ce238d524260fc97754e0c617710a4633a62a
Piper Niehaus <piper.niehaus@gmail.com>
2015-04-25 13:39:19 -0600

    Remove random extra test thing
    [#92751626]

--- spec/features/visit_info_page_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-feature 'user authentication' do
-  scenario 'user can sign up' do
-    visit '/about'
-    expect(page.first('div#test_me').text).to contain('test')
-  end
-end
