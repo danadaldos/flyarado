0f681b5e885cd80e9b0012bb7674976445f4ab22
Piper Niehaus <piper.niehaus@gmail.com>
2015-04-24 19:09:13 -0600

    Limit number of records

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -4,7 +4,7 @@ class StationsController < ApplicationController
   # GET /stations
   # GET /stations.json
   def index
-    @stations = Station.all
+    @stations = Station.find(:all, limit: 5)
   end
 
   # GET /stations/1
