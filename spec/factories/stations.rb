2a6023fbcafad84fd09a5be532dde6cc76fb9dd3
Dana Daldos <danadaldos@gmail.com>
2015-04-28 21:40:29 -0600

    Changing branches

--- spec/factories/stations.rb
+++ spec/factories/stations.rb
@@ -1,8 +1,9 @@
 FactoryGirl.define do
   factory :station do
     name "Orinoco River"
-latitude 7.553027
-longitude -64.9039693
+    latitude 7.553027
+    longitude -64.9039693
+    code 897023
   end
 
 end
--- spec/features/map_feature_spec.rb
+++ spec/features/map_feature_spec.rb
@@ -4,20 +4,20 @@ feature 'Google Maps' do
   before do
     FactoryGirl.create(:station)
   end
-  scenario 'user can see map with a location maker of the station', js: true do
+  scenario 'user can see map with a location maker of the station'  do  #, js: true
     visit '/'
 
-    expect(page).to have_content('Name')
-
-    click_link('New Station')
-
-    fill_in 'Name', with: @station.name
-    fill_in 'Latitude', with: @station.latitude
-    fill_in 'Longitude', with: @station.longitude
-    fill_in 'Code', with: @station.code
-
-    click_button('Create Station')
-
+    # expect(page).to have_content('Name')
+    #
+    # click_link('New Station')
+    #
+    # fill_in 'Name', with: station.name
+    # fill_in 'Latitude', with: station.latitude
+    # fill_in 'Longitude', with: station.longitude
+    # fill_in 'Code', with: station.code
+    #
+    # click_button('Create Station')
+save_and_open_page
     find('#show', :visible => false).click
 
     within('#map') do
