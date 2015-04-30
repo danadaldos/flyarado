502c9beddec3abed732f343cf7878d9060684c7f
Dana Daldos <danadaldos@gmail.com>
2015-04-30 08:58:32 -0600

    Committing before rebase

--- spec/features/weather_spec.rb
+++ spec/features/weather_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
+# This spec uses Webmock to make an API call and, if successful,
+# returns a preset temperature and summary, hence "47.3" and "Clear"
+
 feature 'Weather' do
   before do
     station = Station.create! name: 'Orinoco River', latitude: '7.565515', longitude: '-64.9853958', code: '1234'
@@ -12,6 +15,9 @@ feature 'Weather' do
     expect(page).to have_content("Orinoco")
     expect(page).to have_content("47.3")
     expect(page).to have_content("Clear")
+
+
+    #This section is intended to test the weather on the Show page
 
     #Unable to get Capybara to find "Show" link, even though it's there in the save_and_open_page.
     #page.find('#show', :visible => false).click
