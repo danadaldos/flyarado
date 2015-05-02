f719858c771ed369721b7ba944c6cc71536c2c30
lanterma <lanterma@gmail.com>
2015-05-02 14:45:20 -0600

    No more verify Email
    *Changed user_controller to send welcome email instead of verify email on sign up
    *Added Welcome method
    *changed verify .text and .html view files to welcome.text/html

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -24,18 +24,23 @@ class UserNotifier < ApplicationMailer
   #
   #   en.user_notifier.verified.subject
   #
-  def verified(user)
-    @user = user
-    mail to: @user.email
-
-  end
+  # def verified(user)
+  #   @user = user
+  #   mail to: @user.email
+  #
+  # end
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_notifier.verify.subject
   #
-  def verify(user)
+  # def verify(user)
+  #   @user = user
+  #   mail to: @user.email
+  # end
+
+  def welcome(user)
     @user = user
     mail to: @user.email
   end
